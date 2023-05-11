2a33bbe21be3bd2d07388e053a390b9b45972551
Vijay Dev S S <vijay.dev@shijigroup.com>
2023-05-11 16:46:45 +0530

    Add the ability to create multiple choice question

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -8,22 +8,44 @@ import RadioIcon from '../RadioIcon';
 import CheckIcon from '../CheckIcon';
 import CancelIcon from '../CancelIcon';
 
-export default ({question, name, answer, options}) => {
-  const [disabled, setdisabled] = useState(false);
-  const [choosen, setChoosen] = useState();
+export default ({question, name, answer, answers, choices}) => {
+  const isMultipleChoice = Array.isArray(answers);
+  const [isComplete, setIsComplete] = useState(false);
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [chosen, setChosen] = useState([]);
 
   const onChange = (e) => {
-    setChoosen(e.target.value);
-    setdisabled(true);
+    if (isMultipleChoice) {
+      const uniqueItems = [...new Set([...chosen, e.target.value])];
+
+      setChosen(uniqueItems);
+      setStatus(uniqueItems);
+    } else {
+      setChosen([e.target.value]);
+      setStatus(e.target.value);
+    }
   };
 
-  const noborder = (index) =>
-    options.length % 2 === 0 && index + 2 === options.length;
+  const noBorder = (index) =>
+    choices.length % 2 === 0 && index + 2 === choices.length;
 
   const getIcon = (option) => {
-    if (disabled && option === answer) return <CheckIcon />;
+    const matchAnswer = isMultipleChoice
+      ? answers.includes(option)
+      : option === answer;
+    const matchChosen = isMultipleChoice
+      ? Boolean(chosen.find((item) => option === item))
+      : option === chosen[0];
 
-    if (disabled && option === choosen) return <CancelIcon />;
+    if (isComplete && matchAnswer) {
+      return <CheckIcon isCorrect />;
+    }
+
+    if (isComplete && !isCorrect && matchChosen) {
+      return <CancelIcon />;
+    } else if (isMultipleChoice && matchChosen) {
+      return <CheckIcon />;
+    }
 
     return (
       <>
@@ -33,28 +55,77 @@ export default ({question, name, answer, options}) => {
           name={name}
           value={option}
           onChange={onChange}
-          disabled={disabled}
+          disabled={isComplete}
         />
         <RadioIcon className={styles.input} />
       </>
     );
   };
 
+  const setStatus = (value) => {
+    const selectedItems = value ?? chosen;
+
+    if (isMultipleChoice) {
+      for (let i = 0, j = selectedItems.length; i < j; i++) {
+        const item = selectedItems[i];
+
+        if (!answers.includes(item)) {
+          setIsComplete(true);
+          setIsCorrect(false);
+          break;
+        }
+
+        if (i + 1 === j) {
+          if (j === answers.length) {
+            setIsComplete(true);
+            setIsCorrect(true);
+          } else {
+            setIsComplete(false);
+          }
+        }
+      }
+    } else {
+      setIsComplete(true);
+      setIsCorrect(selectedItems === answer);
+    }
+  };
+
+  const onReset = () => {
+    setIsComplete(false);
+    setIsCorrect(false);
+    setChosen([]);
+  };
+
   return (
     <form
       className={`${styles.form} ${
-        disabled ? (choosen === answer ? styles.success : styles.failed) : ''
+        isComplete ? (isCorrect ? styles.success : styles.failed) : ''
       }`}>
-      <p className={styles.question}>{question}</p>
-      <div className={styles.options}>
-        {options.map((option, index) => (
+      <p className={styles.question}>
+        {question}
+        {isMultipleChoice ? <small>Choose multiple</small> : null}
+      </p>
+      {isComplete ? (
+        <button
+          className={`${styles.info} ${styles.action}`}
+          type="button"
+          onClick={onReset}>
+          Reset
+        </button>
+      ) : (
+        <small className={styles.info}>
+          {isMultipleChoice ? 'Multiple' : 'Single'}
+        </small>
+      )}
+      <div className={styles.choices}>
+        {choices.map((choice, index) => (
           <div
-            key={option}
-            className={`${styles.option} ${
-              noborder(index) ? styles.noBorderBottom : ''
+            key={choice}
+            className={`${styles.choice} ${
+              noBorder(index) ? styles.noBorderBottom : ''
             }`}>
-            {getIcon(option)}
-            <label htmlFor={option}>{option}</label>
+            {getIcon(choice)}
+            <label htmlFor={choice}>{choice}</label>
           </div>
         ))}
       </div>
