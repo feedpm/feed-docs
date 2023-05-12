78c84b6e2afd4d55a70f5abdd89bb6a660cb998b
Vijay Dev S S <vijay.dev@shijigroup.com>
2023-05-12 19:00:17 +0530

    fix the issue with the Quiz component

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,119 +1,123 @@
 /**
  * Use this component to create quiz
  */
-import React, {useState} from 'react';
+import React, {useState, useMemo} from 'react';
 
 import styles from './styles.module.css';
 import RadioIcon from '../RadioIcon';
 import CheckIcon from '../CheckIcon';
 import CancelIcon from '../CancelIcon';
 
-export default ({question, name, answer, answers, choices}) => {
+const Icon = ({
+  name,
+  answer,
+  answers,
+  isMultipleChoice,
+  selection,
+  isComplete,
+  isCorrect,
+  item,
+  onChange,
+}) => {
+  const matchAnswer = isMultipleChoice
+    ? answers.includes(item)
+    : item === answer;
+
+  const matchSelection = selection.includes(item);
+
+  if (isComplete) {
+    if (matchAnswer) {
+      return <CheckIcon isCorrect />;
+    } else if (!isCorrect && matchSelection) {
+      return <CancelIcon />;
+    }
+  }
+
+  if (matchSelection) {
+    return <CheckIcon />;
+  }
+
+  return (
+    <>
+      <input
+        type="radio"
+        id={item}
+        name={name}
+        value={item}
+        onChange={onChange}
+        disabled={isComplete}
+      />
+      <RadioIcon className={styles.input} />
+    </>
+  );
+};
+
+const Quiz = ({question, name, answer, answers, choices}) => {
   const isMultipleChoice = Array.isArray(answers);
-  const [isComplete, setIsComplete] = useState(false);
-  const [isCorrect, setIsCorrect] = useState(false);
-  const [chosen, setChosen] = useState([]);
+  const [userSelection, setUserSelection] = useState([]);
+
+  const [isComplete, isCorrect] = useMemo(() => {
+    const hasSelection = userSelection.length;
+    const allSelected =
+      isMultipleChoice && userSelection.length === answers.length;
+
+    let isComplete = false;
+    let isCorrect = false;
+
+    if (hasSelection) {
+      const allMatched = userSelection.every((item) => answers?.includes(item));
+
+      isComplete = isMultipleChoice
+        ? (allSelected && allMatched) || !allMatched
+        : true;
+
+      if (isComplete) {
+        isCorrect = isMultipleChoice ? allMatched : userSelection[0] === answer;
+      }
+    }
+
+    return [isComplete, isCorrect];
+  }, [answers, answer, userSelection]);
 
   const onChange = (e) => {
     if (isMultipleChoice) {
-      const uniqueItems = [...new Set([...chosen, e.target.value])];
+      const uniqueItems = [...new Set([...userSelection, e.target.value])];
 
-      setChosen(uniqueItems);
-      setStatus(uniqueItems);
+      console.log(uniqueItems);
+
+      setUserSelection(uniqueItems);
     } else {
-      setChosen([e.target.value]);
-      setStatus(e.target.value);
-    }
-  };
-
-  const noBorder = (index) =>
-    choices.length % 2 === 0 && index + 2 === choices.length;
-
-  const getIcon = (option) => {
-    const matchAnswer = isMultipleChoice
-      ? answers.includes(option)
-      : option === answer;
-    const matchChosen = isMultipleChoice
-      ? Boolean(chosen.find((item) => option === item))
-      : option === chosen[0];
-
-    if (isComplete && matchAnswer) {
-      return <CheckIcon isCorrect />;
-    }
-
-    if (isComplete && !isCorrect && matchChosen) {
-      return <CancelIcon />;
-    } else if (isMultipleChoice && matchChosen) {
-      return <CheckIcon />;
-    }
-
-    return (
-      <>
-        <input
-          type="radio"
-          id={option}
-          name={name}
-          value={option}
-          onChange={onChange}
-          disabled={isComplete}
-        />
-        <RadioIcon className={styles.input} />
-      </>
-    );
-  };
-
-  const setStatus = (value) => {
-    const selectedItems = value ?? chosen;
-
-    if (isMultipleChoice) {
-      for (let i = 0, j = selectedItems.length; i < j; i++) {
-        const item = selectedItems[i];
-
-        if (!answers.includes(item)) {
-          setIsComplete(true);
-          setIsCorrect(false);
-          break;
-        }
-
-        if (i + 1 === j) {
-          if (j === answers.length) {
-            setIsComplete(true);
-            setIsCorrect(true);
-          } else {
-            setIsComplete(false);
-          }
-        }
-      }
-    } else {
-      setIsComplete(true);
-      setIsCorrect(selectedItems === answer);
+      setUserSelection([e.target.value]);
     }
   };
 
   const onReset = () => {
-    setIsComplete(false);
-    setIsCorrect(false);
-    setChosen([]);
+    setUserSelection([]);
   };
+
+  const noBorder = (index) =>
+    choices.length % 2 === 0 && index + 2 === choices.length;
 
   return (
     <form
       className={`${styles.form} ${
         isComplete ? (isCorrect ? styles.success : styles.failed) : ''
       }`}>
-      <p className={styles.question}>{question}</p>
-      {isComplete ? (
-        <button
-          className={`${styles.info} ${styles.action}`}
-          type="button"
-          onClick={onReset}>
-          Reset
-        </button>
-      ) : (
-        <small className={styles.info}>
-          {isMultipleChoice ? 'Multiple' : 'Single'}
-        </small>
-      )}
+      <header className={styles.header}>
+        <p className={styles.question}>{question}</p>
+        {isComplete ? (
+          <button
+            className={`${styles.info} ${styles.action}`}
+            type="button"
+            onClick={onReset}>
+            Reset
+          </button>
+        ) : (
+          <small className={styles.info}>
+            {isMultipleChoice ? 'Multiple' : 'Single'}
+          </small>
+        )}
+      </header>
       <div className={styles.choices}>
         {choices.map((choice, index) => (
           <div
@@ -121,7 +125,17 @@ export default ({question, name, answer, answers, choices}) => {
             className={`${styles.choice} ${
               noBorder(index) ? styles.noBorderBottom : ''
             }`}>
-            {getIcon(choice)}
+            <Icon
+              name={name}
+              answer={answer}
+              answers={answers}
+              isMultipleChoice={isMultipleChoice}
+              selection={userSelection}
+              isComplete={isComplete}
+              isCorrect={isCorrect}
+              item={choice}
+              onChange={onChange}
+            />
             <label htmlFor={choice}>{choice}</label>
           </div>
         ))}
@@ -129,3 +143,5 @@ export default ({question, name, answer, answers, choices}) => {
     </form>
   );
 };
+
+export default Quiz;
